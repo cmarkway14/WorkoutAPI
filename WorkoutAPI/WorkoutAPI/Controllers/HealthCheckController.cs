379bd016f4a891b4dbd36a8bcc1431c836232193
Curtis Markway <cmarkway14@users.noreply.github.com>
2022-09-16 14:35:23 -0500

    healthCheck Controller

---
+++ WorkoutAPI/WorkoutAPI/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutAPI.Controllers
+{
+    [ApiController]
+    public class HealthCheckController : Controller
+    {
+        [Route("api/[controller]/check")]
+        public ActionResult Health()
+        {
+            return Ok(DateTime.Now.ToString("MM/dd/y h:mm tt"));
+        }
+    }
+}
