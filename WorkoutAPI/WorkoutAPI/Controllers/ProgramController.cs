e79bcdb2eabd775adc34be45ff79702a2b20c569
Curtis Markway <cmarkway14@users.noreply.github.com>
2022-09-16 15:34:50 -0500

    working on getting a single program

---
+++ WorkoutAPI/WorkoutAPI/Controllers/ProgramController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/")]
+    public class ProgramController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult> Program()
+        {
+            return Ok(Task.FromResult(true));
+        }
+    }
+}
