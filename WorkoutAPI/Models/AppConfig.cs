b59ebb86bc51e5298d3b345415fd87f3bc6989d5
Curtis Markway <cmarkway14@users.noreply.github.com>
2022-09-16 15:25:45 -0500

    adding health check around mongoDB connection

---
+++ WorkoutAPI/Models/AppConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Models
+{
+    public class AppConfig : IAppConfig
+    {
+        public string MongoClientConnection { get; set; }
+    }
+}
--- WorkoutAPI/WorkoutAPI/Controllers/HealthCheckController.cs
+++ WorkoutAPI/WorkoutAPI/Controllers/HealthCheckController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace WorkoutAPI.Controllers
 {
@@ -6,9 +8,46 @@ namespace WorkoutAPI.Controllers
     public class HealthCheckController : Controller
     {
         [Route("api/[controller]/check")]
+        [HttpGet]
         public ActionResult Health()
         {
-            return Ok(DateTime.Now.ToString("MM/dd/y h:mm tt"));
+            var mongoDBStatus = CheckMongoDbHealth();
+
+            if (mongoDBStatus)
+            {
+                return Ok($"MongoDB is up as of: {DateTime.Now.ToString("MM/dd/y h:mm tt")}");
+            }
+
+            return StatusCode(400, "MongoDB is not up, you might need to go reboot it");
         }
+
+        private bool CheckMongoDbHealth()
+        {
+            try
+            {
+                var conn = "mongodb+srv://Cmarkway14:<PASSWORD>@workoutdb.nxwwqq9.mongodb.net/?retryWrites=true&w=majority";
+                var client = new MongoClient(conn);
+                var workoutDB = client.GetDatabase("Workout");
+
+                var healthCollection = workoutDB.GetCollection<MongoDdHealth>("Health");
+                var healthResult = healthCollection.Find(Builders<MongoDdHealth>.Filter.Empty).ToList();
+
+                if (healthResult.First().CanRead)
+                {
+                    return true;
+                }
+                return false;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+
+    public class MongoDdHealth
+    {
+        public ObjectId _id { get; set; }
+        public bool CanRead { get; set; }
     }
 }
